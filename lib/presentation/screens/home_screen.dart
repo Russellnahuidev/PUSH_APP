e86a27607a87dd5653951390d061bec511ca2d1d
Russell ÑAHUI <russellnahui.dev25@gmail.com>
2025-06-17 19:34:27 -0500

    Creando la app, configurnado el tema y las rutas iniciales

---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Permisos'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // Todo: Solicitar permisos
+            },
+            icon: Icon(Icons.settings),
+          ),
+        ],
+      ),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
